4d6d13fdddcdf64a3e4fadf431aaf0dea42ec01b
7Romuch7 <64313145+7Romuch7@users.noreply.github.com>
2020-11-05 14:08:17 +0200

    Корректировка промисов

--- src/task-03.js
+++ src/task-03.js
@@ -5,27 +5,27 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
+const makeTransaction = ({id: transaction} ) => {
     const delay = randomIntegerFromInterval(200, 500);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const canProcess = Math.random() > 0.3;
 
             if (canProcess) {
-                resolve(transaction.id, delay);
+              resolve({id: transaction, time: delay});
             } else {
-                reject(transaction.id);
+              reject({id: transaction});
             }
     }, delay);
     });
 }
 
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
-const logError = id => {
+const logError = ({ id }) => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
